463f9a128d69d08c211a235044d4c1df0529f5ed
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-01 21:45:29 -0300

    all seven tests passed in exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+ const repeatString = function(string, num) {
+    let repeatStrings="";
+    
+        if(num >=0){
+            while(num >0){
+      repeatStrings += string;
+      num --;
+    }
+    }else{
+        repeatStrings = "ERROR";
+    }
 
+
+    return repeatStrings;
+    
 };
-
+const number = Math.floor(Math.random() * 10)
+    num =number;
+repeatString('', num);
 // Do not edit below this line
 module.exports = repeatString;
