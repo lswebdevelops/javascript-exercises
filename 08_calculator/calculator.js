7eb8ce5ef4948293ad3cc1aebd1e1cacf5a47b36
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-15 16:24:52 -0300

    all 16 tests passed on calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,54 @@
-const add = function() {
+function add (num1, numb2){
+  let sum = num1 + numb2;
+   let result = typeof(summ);
+   console.log(result)
+
+  if(result !== 'number'){
+
+      
+}
+ return sum;
+}
+
+const subtract = function( num1, num2) {
+	let result = num1- num2;
+  return result;
+};
+
+const sum = function(sum) {
+	 	   
+  const array = sum;
+  let summ = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+      summ += array[i];
+  }
+  return summ;
+};
+
+const multiply = function(multiplies) {
+   	   
+  const array = multiplies;
+  let mult = 1;
+  
+  for (let i = 0; i < array.length; i++) {
+      mult *= array[i];
+  }
+  return mult;
+};
+
+const power = function(num, num2) {
+  return num ** num2;
 	
 };
 
-const subtract = function() {
-	
+const factorial = function(n) {
+	if(n === 1 || n === 0)
+  return 1;
+  else
+  return n * factorial(n-1)
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
@@ -31,3 +59,6 @@ module.exports = {
   power,
   factorial
 };
+
+
+
