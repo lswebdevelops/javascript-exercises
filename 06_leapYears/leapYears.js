1393d06ecec43103045e1d7bf80c621ec01322a3
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-15 10:55:46 -0300

    all tests passed on the leap year exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+
+if(year % 400 === 0 || year % 4 === 0 && year % 100 !== 0){
+    return true;
+}
+
+   return false;
+     
 
 };
 
+
+console.log(leapYears(2024));
+
+
+
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
+
