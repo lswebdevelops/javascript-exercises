1e8554371256758d11d66ffa7a17e02261874348
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-15 11:51:48 -0300

    all tests passed on the Farenheit to Celsius conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,41 @@
-const ftoc = function() {
+const ftoc = function(fTemp) {
+  
+  // C = 5/9 x (F-32)
+
+ let f_to_C = 0;
+ f_to_C = 5/9 * (fTemp - 32);
+
+ let roundedC = Math.round(f_to_C *10)/10 ;
+ 
+ return roundedC;
+
 
 };
+console.log(ftoc(303));
 
-const ctof = function() {
 
+
+const ctof = function(cTemp) {
+
+  //F = 1.8 C + 32  or F = 9/5 C + 32
+
+  let c_to_F = 0;
+  c_to_F = 9/5 * cTemp + 32 ;
+
+   let roundedF = Math.round(c_to_F *10)/10 ;
+ 
+  return roundedF;
 };
+
+
+console.log(ctof(1545));
 
 // Do not edit below this line
 module.exports = {
   ftoc,
   ctof
 };
+
+
+
+
