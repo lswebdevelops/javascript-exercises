2415c09856a2fa92e3e29aea56601f96f0bb6cf5
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-15 09:58:53 -0300

    all tests passed on exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,46 @@
-const sumAll = function() {
+  const sumAll =function(firstNumber, lastNumber) {
 
+  let f = firstNumber;
+  let l = lastNumber;
+
+  if('number' !== typeof(f))
+{  return "ERROR";}
+
+if('number' !== typeof(l))
+{  return "ERROR";}
+
+
+
+    if(f < 0){
+        return "ERROR";
+    }
+    
+    if(firstNumber > lastNumber ){
+       f =lastNumber;
+       l = firstNumber;
+    }
+
+
+
+
+    let arr =[]  ;
+    while(f < l +1){
+        arr.push(f++);
+    }
+    
+   
+    
+    
+    let sum = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    
+
+return sum;
 };
 
+ console.log(sumAll(123,1))
 // Do not edit below this line
 module.exports = sumAll;
