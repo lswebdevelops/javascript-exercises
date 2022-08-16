7bb57e9c1253b827f07728e148c456e8af13e97a
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-16 06:01:47 -0300

    all tests passed on the object/array exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+
+  const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ];
+  const getTheTitles = function() {
+
+    let newArray = [];
+
+    newArray.push(books[0].title)
+    newArray.push(books[1].title);
+    
+        return newArray;
 
 };
-
+console.log(typeof(getTheTitles(books)));
 // Do not edit below this line
 module.exports = getTheTitles;
