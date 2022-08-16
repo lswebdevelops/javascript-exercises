78a29f8ec3da5d81b4c0a9acc5c0d17ce612e147
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-08-15 22:56:22 -0300

    all test passed on fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,42 @@
-const fibonacci = function() {
 
-};
+
+const fibonacci = function(n) {
+
+   
+    if(n < 1){
+        return "OOPS";
+    
+    }
+    else if(n == 1){
+        return 1;
+    }
+    else
+    {
+    
+        function fibonacciArray (n) {
+      
+            let fib = [0, 1];
+            let data = [];
+            
+            for(let i = 2; i <= n; i++) {
+              fib[i] = fib[i - 1] + fib[i - 2]; 
+              data.push(fib[i]);
+            }
+            
+            return data;
+          }
+        
+    let result = fibonacciArray(n);
+    
+    let lastItem = result.slice(-1).pop();
+    console.log(`The result of the fibonacci of ${n} in ${lastItem}`)
+    return lastItem;
+    
+        }
+    
+    }
+    console.log(fibonacci(-25));
+    
 
 // Do not edit below this line
 module.exports = fibonacci;
