5e9d144d52c2e953cc9050bc8d97e571c4eee833
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-03-11 20:53:30 -0300

    arrays Ascending sorted and descending sorted

---
+++ sorting/index.js
@@ -0,0 +1,41 @@
+const arr = [1, 3, 5, 33, 82, 2, 5];
+
+function ascendingSorting(list){
+    const newArray = [];
+    for(let i = 0; i < list.length; i++) {
+        for(let j = i + 1; j < list.length; j++) {
+          if(list[i] > list[j]) {
+            let temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+          }
+        }
+      }
+      newArray.push(...list);
+      return newArray;
+      
+}
+function descendingSorting(list){
+    const newArray = [];
+    for(let i = 0; i < list.length; i++) {
+        for(let j = i + 1; j < list.length; j++) {
+          if(list[i] < list[j]) {
+            let temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+          }
+        }
+      }
+      newArray.push(...list);
+      return newArray;
+      
+}
+
+
+console.log(`Ascending sorted: ${ascendingSorting(arr)}`);;
+console.log(`Descending sorted: ${descendingSorting(arr)}`);;
+
+
+
+
+
