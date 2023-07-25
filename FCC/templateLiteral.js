5a07f1bd9419396014478dc7769a6abf44230d77
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-25 13:28:33 -0300

    template literal

---
+++ FCC/templateLiteral.js
@@ -0,0 +1,38 @@
+// const person = {
+//     name: "Zodiac Hasbro",
+//     age: 56
+//   };
+  
+//   const greeting = `Hello, my name is ${person.name}! 
+//   I am ${person.age} years old.`;
+  
+//   console.log(greeting);
+
+
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = arr.map(item =>`<li class="text-warning">${item}</li>`
+    )
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
+console.log(failuresList);
+
+// 0
+// : 
+// "<li class=\"text-warning\">no-var</li>"
+// 1
+// : 
+// "<li class=\"text-warning\">var-on-top</li>"
+// 2
+// : 
+// "<li class=\"text-warning\">linebreak</li>"
