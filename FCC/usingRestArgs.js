5165f8ea776adb0013cdf2c4f9ccaf56b46064b7
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-25 09:19:07 -0300

    using rest + arguments

---
+++ FCC/usingRestArgs.js
@@ -0,0 +1,23 @@
+const sum = (...args) => {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+};
+
+console.log(`total: ${sum(0, 1, 2)}`);
+
+
+
+function howMany(...args) {
+  return "You have passed " + args.length + " arguments.";
+}
+console.log(howMany(0, 1, 2));
+console.log(howMany("string", null, [1, 2, 3], {}));
+
+function howManyArguments(...argument) {
+  return `number of arguments: ${argument.length}`;
+}
+
+console.log(howManyArguments("as", null, false, 2));
