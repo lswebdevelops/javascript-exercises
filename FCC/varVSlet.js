d29160d62edae3c80fd97b68fccd631aa735ac22
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-25 08:44:36 -0300

    incrementing

--- FCC/varVSlet.js
+++ FCC/varVSlet.js
@@ -5,7 +5,6 @@
 // console.log(numArray);
 // console.log(i);
 
-
 // var numArraj = [];
 // var j;
 // for (j = 0; j < 3; j++) {
@@ -13,9 +12,6 @@
 // }
 // console.log(numArraj);
 // console.log(j);
-
-
-
 
 // var printNumTwo;
 // for (let i = 0; i < 3; i++) {
@@ -39,21 +35,59 @@
 
 //   checkScope()
 
-
 // const s = [5, 6, 7];
 // // s = [1, 2, 3];this is invalid
 // s[2] = 45;
 // console.log(s);// [5,6,45]
 
+// let obj = {
+//     name:"FreeCodeCamp",
+//     review:"Awesome"
+//   };
+//   obj.review = "bad";
+//   obj.newProp = "Test";
+//   Object.freeze(obj); // this freezes the variable not allowing new edits to it
+//   obj.newAppendNotAllowed; // { name: 'FreeCodeCamp', review: 'bad', newProp: 'Test
 
-let obj = {
-    name:"FreeCodeCamp",
-    review:"Awesome"
-  };
-  obj.review = "bad";
-  obj.newProp = "Test";
-  Object.freeze(obj); // this freezes the variable not allowing new edits to it
-  obj.newAppendNotAllowed; // { name: 'FreeCodeCamp', review: 'bad', newProp: 'Test
+//   console.log(obj);
 
-  console.log(obj);
+// //   for example:
+//   function freezeObj() {
+//     const MATH_CONSTANTS = {
+//       PI: 3.14
+//     };
+//     // Only change code below this line
+//   Object.freeze(MATH_CONSTANTS)
 
+//     // Only change code above this line
+//     try {
+//       MATH_CONSTANTS.PI = 99;
+//     } catch(ex) {
+//       console.log(ex);
+//     }
+//     return MATH_CONSTANTS.PI;
+//   }
+//   const PI = freezeObj();
+
+//   var  myConcat = (arr1, arr2) => {
+//     return arr1.concat(arr2);
+//   };
+
+//   console.log(myConcat([1, 2], [3, 4, 5]));
+
+// parameters for function
+
+// const greeting = (name = "Anonymous", greet = "Hi ") => greet + name + "!";
+
+// console.log(greeting("John", "Hello "));// Hello John
+// console.log(greeting()); // Hello Anonymous
+
+
+
+const increment = (number, value = 1) => number + value;
+
+console.log(increment(5, 2)); // returns 7
+
+console.log(increment(5)); // returns 6
+
+// Only change code above this line
