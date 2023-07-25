e6a0f84226e5f1e425f7aacb7a7da822d5f63ecc
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-25 14:03:09 -0300

    Write Concise Declarative Functions with ES

--- FCC/consiveObjectLeteralDeclaration.js
+++ FCC/consiveObjectLeteralDeclaration.js
@@ -35,12 +35,36 @@ const person = {
 };
 console.log(person.sayHello());
 
-
 const person2 = {
-    name: "Luciano",
-    sayHello() {
-        return `Hello! My name is ${this.name}`
-    }
-}
+  name: "Luciano",
+  sayHello() {
+    return `Hello! My name is ${this.name}`;
+  },
+};
 
 console.log(person2.sayHello());
+
+// before
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear: function (newGear) {
+    this.gear = newGear;
+  },
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(`gear: ${bicycle.gear}`);
+
+//after:
+
+// Only change code below this line
+const bicycle2 = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  },
+};
+// Only change code above this line
+bicycle2.setGear(3);
+console.log(`gear: ${bicycle2.gear}`);
