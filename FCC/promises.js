310e359a15a766c6f870f05f1973bbc1e1a10603
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-07-26 08:54:11 -0300

    using regex to test if a work is there or not

--- FCC/promises.js
+++ FCC/promises.js
@@ -1,48 +1,46 @@
 const makeServerRequest = new Promise((resolve, reject) => {
-    // responseFromServer represents a response from a server
-    let responseFromServer;
-      
-    if(responseFromServer) {
-      // Change this line
-      resolve("We got the data")
-    } else {  
-      // Change this line
-      reject("Data not received")
-    }
-  });
-  
+  // responseFromServer represents a response from a server
+  let responseFromServer;
 
-  const makeServerRequest2 = new Promise((resolve, reject) => {
-    // responseFromServer is set to true to represent a successful response from a server
-    let responseFromServer = true;
-      
-    if(responseFromServer) {
-      resolve("We got the data");
-    } else {  
-      reject("Data not received");
-    }
-  });
-  
-  makeServerRequest.then(result => {
-    console.log(result)
-  })
+  if (responseFromServer) {
+    // Change this line
+    resolve("We got the data");
+  } else {
+    // Change this line
+    reject("Data not received");
+  }
+});
 
+const makeServerRequest2 = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
 
-  const makeServerRequest3 = new Promise((resolve, reject) => {
-    // responseFromServer is set to false to represent an unsuccessful response from a server
-    let responseFromServer = false;
-      
-    if(responseFromServer) {
-      resolve("We got the data");
-    } else {  
-      reject("Data not received");
-    }
-  });
-  
-  makeServerRequest.then(result => {
-    console.log(result);
-  });
-  
-  makeServerRequest.catch(error => {
-    console.log(error)
-  })
+  if (responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then((result) => {
+  console.log(result);
+});
+
+const makeServerRequest3 = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+
+  if (responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then((result) => {
+  console.log(result);
+});
+
+makeServerRequest.catch((error) => {
+  console.log(error);
+});
---
+++ FCC/regex.js
@@ -0,0 +1,8 @@
+let testStr = "freeCodeCamp";
+let testRegex = /Code/;
+let testRegex2 = /code/;
+console.log(testRegex.test(testStr)); // true
+console.log(testRegex2.test(testStr)); // false
+
+
+
